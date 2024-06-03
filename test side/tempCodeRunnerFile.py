5636beac6301cccf5b1312c61b31e83121b25826
A-T-O-M-2 <k.raanapramodh7@gmail.com>
2024-06-03 16:45:33 +0530

    Delete test side directory

--- test side/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-        #     # Get all passage-based questions for the English section
-        #     passages_with_questions = get_passages_with_questions()
-        #     sections_with_questions[section] = passages_with_questions
-        # else:
