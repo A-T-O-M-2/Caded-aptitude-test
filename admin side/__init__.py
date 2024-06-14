7c7160c9d3eadf328d458399b1753748a18ba754
A-T-O-M-2 <k.raanapramodh7@gmail.com>
2024-06-15 00:17:19 +0530

    Delete admin side directory

--- admin side/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_wtf.csrf import CSRFProtect
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-csrf = CSRFProtect(app)
-
-from app import routes
